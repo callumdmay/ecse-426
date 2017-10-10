4dc72abafb68a45afa7d50864f49a50d3821d40f
callumdmay <callumdmay@gmail.com>
2017-10-10 14:05:20 -0400

    Add ARM file

--- Lab1/Sources/main.c
+++ Lab1/Sources/main.c
@@ -3,18 +3,29 @@
 #include "arm_math.h"
 
 int IIR_C(float* InputArray, float* OutputArray, float* coeff, int Length, int Order);
-	
+extern int IIR_asm(float* inputArray, float* OutputArray, int Lenfth, float_ coeff)
+
+
 int main() {
     float inputArray[5] = {1,2,3,4,5};
-    float outputArray[5];
-    int length = 5;
+		int length = 5;
+		int order = 2;
+		float outputArray[5];
 
-    int order = 2;
     float coeffArray[5] = {0.1,0.15,0.5,0.15,0.1};
 
     IIR_C(inputArray, outputArray, coeffArray, length, order);
+		int i;
+		for (i = 0; i < length; i++) {
+			printf("%f ", outputArray[i]);
+		}
+		printf("\n Arm Output:\n");
+    IIR_asm(inputArray, outputArray, coeffArray, length, order);
+		for (i = 0; i < length; i++) {
+			printf("%f ", outputArray[i]);
+		}
 
-	return 0;
+		return 0;
 }
 
 
