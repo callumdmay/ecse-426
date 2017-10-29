fdd44cb1c5514b2f178e5d7133deb0fa500d4b7c
callumdmay <callumdmay@gmail.com>
2017-10-29 16:07:08 -0400

    Beautify code in lab 2

--- Lab2/Src/main.c
+++ Lab2/Src/main.c
@@ -5,7 +5,7 @@
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
@@ -71,101 +71,54 @@ struct Buff_t;
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
+//Always set our window size to 1000, can adjust virtual window size inside of it
 int Window = 1000;
 int value_array[4];
-int f2=0;
-int f=0;
-int z=0;
-int w=0;
-int c=0;
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{ 
-	if	(htim->Instance==TIM2)
-	{
-		f2=1;
-		z+=1;
-	}
-	
-	
-	if(htim->Instance==TIM3)
-	{
-		w+=1;
-		if (w>=2)
-		{
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
-		
-		w=0;
-		}
-		f=1;
-		if (c>=3)
-			{
-			c=0;
-			}
-		else {c+=1;}
-	}
+int f2 = 0;
+int f = 0;
+int z = 0;
+int w = 0;
+int c = 0;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim) {
+  if (htim - > Instance == TIM2) {
+    f2 = 1;
+    z += 1;
+  }
+
+  if (htim - > Instance == TIM3) {
+    w += 1;
+    if (w >= 2) {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
+      w = 0;
+    }
+    f = 1;
+    if (c >= 3) {
+      c = 0;
+    } else {
+      c += 1;
+    }
+  }
 }
-
-
-/*
-typedef struct {uint8_t* const buffer;
-								int head;
-								int tail;
-								const int length;
-} Buff_t;
-
-int BuffPush(Buff_t *b, uint8_t value)
-	{
-		int next = b->head + 1;
-		if (next >= b->length )
-			{
-				next=0;
-			}
-		b->buffer[b->head] = value;
-		b->head = next;
-		return 0;
-	}
-
-int BuffPop (Buff_t *b, uint8_t *value)
-	{
-		int next = b->tail +1;
-		if(next >= b->length)
-			{
-				next = 0;
-			}
-		*value = b->buffer[b->tail];
-			b->tail=next;
-		return 0;
-	}
-*/
-
-
 /* USER CODE END 0 */
-
-int main(void)
-	{
-
+int main(void) {
   /* USER CODE BEGIN 1 */
-float varA=0;
-float varB=0;
-int N = 0;	
-int value;
-float	conv;
-float conv2;
+  float varA = 0;
+  float varB = 0;
+  int N = 0;
+  int value;
+  float conv;
+  float conv2;
 
-uint8_t adcVal;
-float RMS;
-float RMS2;
-uint8_t prev;
-uint8_t temp;
+  uint8_t adcVal;
+  float RMS;
+  float RMS2;
+  uint8_t prev;
+  uint8_t temp;
 
-uint8_t Buff_dataSpace[Window];
+  uint8_t Buff_dataSpace[Window];
 
-uint8_t Buffer[1000];
-int in=0, out=0;
-
-
-
-
+  uint8_t Buffer[1000];
+  int in = 0, out = 0;
   /* USER CODE END 1 */
 
   /* MCU Configuration----------------------------------------------------------*/
@@ -191,130 +144,101 @@ int in=0, out=0;
   MX_TIM3_Init();
 
   /* USER CODE BEGIN 2 */
-HAL_TIM_Base_Start_IT(&htim2);
-HAL_TIM_Base_Start_IT(&htim3);
-HAL_ADC_Start(&hadc2);
-
+  HAL_TIM_Base_Start_IT( & htim2);
+  HAL_TIM_Base_Start_IT( & htim3);
+  HAL_ADC_Start( & hadc2);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-  /* USER CODE END WHILE */
+  while (1) {
+    /* USER CODE END WHILE */
 
-  /* USER CODE BEGIN 3 */
-		
-		while (f2==1)
-		{
-					adcVal = HAL_ADC_GetValue(&hadc2);
-			
-				
-		if (N >= Window)
-			{
-				//BuffPop (&Buff, &prev);
-				prev = Buffer[out];
-				out = (out+1)%(Window);
-				conv2 = (((3.300)*prev)/255);
-				varB = varB + ((conv2)*(conv2));
-			}
-		else
-			{
-				N+=1;
-			}	
-			//BuffPush (&Buff, adcVal);
-			Buffer[in]=adcVal;
-			in = (in+1)%(Window);
-				conv = (((3.300)*adcVal)/255);
-				varA = varA +((conv)*(conv));
-		
+    /* USER CODE BEGIN 3 */
 
-	
-			RMS2 = ((varA)-(varB))/N;
-			RMS = sqrtf(RMS2);
-while (z>=120){
-			value = (int)(1000*RMS);
-	for (int v = 0; v < 4; v++) 
-				{
-					value_array[3-v] = value%10;
-					value = value/10;
-				}
-				
-	z=0;
-}
-			if ((int)varA >= INT_MAX-130000)
-				{
-					varA = 2*RMS;
-					varB = RMS;
-				} 
-			
-			
-			
-			
-			f2=0;
-		}
-			
-		while(f==1){
-			
-					
-			switch (c)
-			{
-				case 0:
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_RESET); 	//digit 4 off
-					Segments (c, value_array[c]);
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_SET);	 	//digit 1 on
-				break;
-					
-				case 1:
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_RESET); 	//digit 1 off
-				Segments (c, value_array[c]);
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET);	 	//digit 2 on
-				break;
-				
-				case 2:
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET);	//digit 2 off
-				Segments (c, value_array[c]);
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_SET);		//digit 3 on
-				break;
-				
-				case 3:
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_RESET);	//digit 3 off
-				Segments (c, value_array[c]);
-					HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_SET);		//digit 4 on
-				break;
-			}
-		
-			
-		
-		
-		f=0;
-		}
-					
-		
+    while (f2 == 1) {
+      adcVal = HAL_ADC_GetValue( & hadc2);
 
-// Step(1): To get the ADC value
-// Step(2): Display ADC value on LEDs
-// 1. adc val 0 - 63, Green LED
-if (adcVal == 0) 
-		{
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-		
-		}
-// 2. adc val 64 -127, Orange LED
-else 
-{
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+      if (N >= Window) {
+        //BuffPop (&Buff, &prev);
+        prev = Buffer[out];
+        out = (out + 1) % (Window);
+        conv2 = (((3.300) * prev) / 255);
+        varB = varB + ((conv2) * (conv2));
+      } else {
+        N += 1;
+      }
+      //BuffPush (&Buff, adcVal);
+      Buffer[ in ] = adcVal; in = ( in +1) % (Window);
+      conv = (((3.300) * adcVal) / 255);
+      varA = varA + ((conv) * (conv));
 
-}
+      RMS2 = ((varA) - (varB)) / N;
+      RMS = sqrtf(RMS2);
+      while (z >= 120) {
+        value = (int)(1000 * RMS);
+        for (int v = 0; v < 4; v++) {
+          value_array[3 - v] = value % 10;
+          value = value / 10;
+        }
 
+        z = 0;
+      }
+      if ((int) varA >= INT_MAX - 130000) {
+        varA = 2 * RMS;
+        varB = RMS;
+      }
 
+      f2 = 0;
+    }
+
+    while (f == 1) {
+
+      switch (c) {
+      case 0:
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_RESET); //digit 4 off
+        Segments(c, value_array[c]);
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_SET); //digit 1 on
+        break;
+
+      case 1:
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_RESET); //digit 1 off
+        Segments(c, value_array[c]);
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET); //digit 2 on
+        break;
+
+      case 2:
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET); //digit 2 off
+        Segments(c, value_array[c]);
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_SET); //digit 3 on
+        break;
+
+      case 3:
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_RESET); //digit 3 off
+        Segments(c, value_array[c]);
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_SET); //digit 4 on
+        break;
+      }
+      f = 0;
+    }
+
+    // Step(1): To get the ADC value
+    // Step(2): Display ADC value on LEDs
+    // 1. adc val 0 - 63, Green LED
+    if (adcVal == 0) {
+      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+
+    }
+    // 2. adc val 64 -127, Orange LED
+    else {
+      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+
+    }
   }
-
   /* USER CODE END 3 */
-
 }
 
 /** System Clock Configuration
@@ -325,13 +249,13 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Configure the main internal regulator output voltage 
+    /**Configure the main internal regulator output voltage
     */
   __HAL_RCC_PWR_CLK_ENABLE();
 
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -347,7 +271,7 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -361,11 +285,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -375,93 +299,56 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
+void Segments(int digit, int value) {
 
+  if ((value != 1) && (value != 4)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET); //reset A
+  }
 
-void Segments (int digit, int value)
-{
-	
-	if( (value != 1) && (value != 4))
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET);//reset A
-		}
-	
-	if( (value != 5) && (value != 6))
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_RESET);//reset B
-		}
-	
-	if( (value != 2) )
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET);//reset C
-		}
-	
-	if( (value != 1) && (value != 4) && (value != 7) && (value != 9))
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_SET);
-		}	
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_RESET);//reset D
-		}
-	
-	if( (value == 2) || (value == 6) || (value == 8) || (value == 0))
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET);//reset E
-		}
-	
-	if( (value != 1) && (value != 2) && (value != 3) && (value != 7))
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, GPIO_PIN_SET);
-		}	
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, GPIO_PIN_RESET);//reset F
-		}
-	
-	if( (value != 1) && (value != 7) && (value != 0) )
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, GPIO_PIN_RESET);//reset G
-		}
-		
-		
-	
-	
-	if ( digit == 0) 
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, GPIO_PIN_SET);
-		}
-	else
-		{
-			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, GPIO_PIN_RESET);
-		}
+  if ((value != 5) && (value != 6)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_RESET); //reset B
+  }
 
+  if ((value != 2)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET); //reset C
+  }
 
+  if ((value != 1) && (value != 4) && (value != 7) && (value != 9)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_RESET); //reset D
+  }
 
+  if ((value == 2) || (value == 6) || (value == 8) || (value == 0)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_6, GPIO_PIN_RESET); //reset E
+  }
 
+  if ((value != 1) && (value != 2) && (value != 3) && (value != 7)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_7, GPIO_PIN_RESET); //reset F
+  }
 
+  if ((value != 1) && (value != 7) && (value != 0)) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, GPIO_PIN_RESET); //reset G
+  }
 
+  if (digit == 0) {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, GPIO_PIN_SET);
+  } else {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_9, GPIO_PIN_RESET);
+  }
 }
-
 /* USER CODE END 4 */
 
 /**
@@ -473,10 +360,10 @@ void _Error_Handler(char * file, int line)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  while(1) 
+  while(1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */ 
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -501,10 +388,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-*/ 
+*/
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
