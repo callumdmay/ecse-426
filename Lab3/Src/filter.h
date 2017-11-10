e82f12bfaa1fb5ef9a3821504c476d6f49c81878
ALAMS_PC\Alam <alamjyot1@gmail.com>
2017-11-10 02:47:54 -0500

    no message

--- Lab3/Src/filter.h
+++ Lab3/Src/filter.h
@@ -1,7 +1,7 @@
 #ifndef FILTER
 #define FILTER
 
-float IIR(float* InputArray, float* coeff, int Length, int Order);
+float IIR(float* InputArray, float* outputArray);
 void filterValues(float rawValues[3], float filteredValues[3] );
 
 
